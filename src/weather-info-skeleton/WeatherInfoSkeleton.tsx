2c617d29bd105abf442325f04476444acf5d68d6
Ati0199 <art019900@mail.ru>
2026-04-23 23:36:01 +0400

    Weather-app

---
+++ src/weather-info-skeleton/WeatherInfoSkeleton.tsx
@@ -0,0 +1,9 @@
+import "./WeatherInfoSkeleton.scss";
+
+export default function WeatherInfoSkeleton() {
+  return (
+    <div className="weather-info-skeleton">
+      <span>loading...</span>
+    </div>
+  );
+}
